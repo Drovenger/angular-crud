8477dd3565ce7cb3d674b8b17d1bfaf67a627960
Drovenger <64911983+Drovenger@users.noreply.github.com>
2020-09-30 23:35:51 +0700

    loi

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular9-springboot-product';
+  title = 'Angular 9 CRUD';
 }
